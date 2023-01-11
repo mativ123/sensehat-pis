0753bcaac33f9fcb9be1dc992792e82b7e2e9049
alfredksigsgaard <alfredksigsgaard@gmail.com>
2023-01-11 13:16:09 +0100

    Update matematik.py

--- matematik.py
+++ matematik.py
@@ -1,18 +1,43 @@
 import time
+st=time.time()
 
-pt = 0
-ct = 0
+#matematik til hastighed
+pt = st
+ct = st
 dt = 0
 
 a = int(input())
 v = 0
 
-bo=time.time()
+
+vs=0
+
+john=0
+
+#tankmåler
+tkmk=int((input("tast hvor meget")))
+kml=22
+ml=kml*1000
+tank=5
+
 
 while(True):
     pt = ct
     ct = time.time()
     dt = (ct - pt)
     v += a * dt
+    if a!=0:
+        vs=v
 
-    print(v -bo)
+    #print(vs)
+    john+=vs*dt
+    #print(john)
+    #tankmålerting
+    
+    mathias = john/ml
+    #print(mathias)
+    tank=tank-(mathias/1000)
+    print(tank)
+    #print(mathias)
+
+
