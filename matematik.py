bfcaaa0bd50cc16b83d951b4d9a7b85c6d8eb53c
alfredksigsgaard <alfredksigsgaard@gmail.com>
2023-01-18 09:54:53 +0100

    Update matematik.py

--- matematik.py
+++ matematik.py
@@ -1,43 +1,27 @@
 import time
-st=time.time()
 
-#matematik til hastighed
-pt = st
-ct = st
-dt = 0
+# Get input for tank measurement (l)
+fuel = int(input("Enter amount for tank measurement: "))
 
-a = int(input())
-v = 0
+# Set fuel efficiency (km/l)
+kml = 22
 
+# Get input for acceleration (m/s^2)
+a = int(input("Enter acceleration:"))
 
-vs=0
+# Initialize speed (m/s), distance (m) and time, previous time 
+v, d, t,pt = 0, 0, fuel, time.time()
 
-john=0
-
-#tankmåler
-tkmk=int((input("tast hvor meget")))
-kml=22
-ml=kml*1000
-tank=5
-
-
-while(True):
-    pt = ct
+while True:
     ct = time.time()
-    dt = (ct - pt)
+    dt = ct-pt
+    pt = ct
     v += a * dt
-    if a!=0:
-        vs=v
-
-    #print(vs)
-    john+=vs*dt
-    #print(john)
-    #tankmålerting
-    
-    mathias = john/ml
-    #print(mathias)
-    tank=tank-(mathias/1000)
-    print(tank)
-    #print(mathias)
-
-
+    v_km_per_s = v / 1000
+    d += v_km_per_s * dt
+    fuel_consumption = v_km_per_s * dt / kml
+    t -= fuel_consumption
+    print("Speed: {:.2f} km/h Distance: {:.2f} km Fuel: {:.2f} L".format(v, d, t))
+    if t <= 0:
+        print("Fuel is empty, the journey has ended.")
+        break
